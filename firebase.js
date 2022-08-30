d7664dc6b623febf567ed7925713f19c0f945840
sandyzhang1010 <zhangdibj@163.com>
2022-08-30 11:56:50 +1000

    day 3

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import friebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDknS2PduCGIgk69SHyraxhge2D69HIYYI",
+  authDomain: "fir-19e91.firebaseapp.com",
+  projectId: "fir-19e91",
+  storageBucket: "fir-19e91.appspot.com",
+  messagingSenderId: "1074680512275",
+  appId: "1:1074680512275:web:8d27f103616306f3c464e6"
+}
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,16 +2,26 @@ import Image from "next/image"
 import { useState } from "react"
 import { StarIcon } from '@heroicons/react/solid'
 import Currency from 'react-currency-formatter'
+import { useDispatch } from "react-redux"
+import { addToBasket } from "../slices/basketSlice"
 
 const MAX_RATING = 5
 const MIN_RITING = 1
 
 function Product ({ id, title, price, description, category, image }) {
+  const dispatch = useDispatch()
 
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RITING + 1)) + MIN_RITING
   )
   const [hasPrime] = useState(Math.random() < 0.5)
+
+  const addItemToBasket = () => {
+    const product = {
+      id, title, price, rating, description, category, image, hasPrime
+    }
+    dispatch(addToBasket(product))
+  }
 
   return (
     <div className=" relative flex flex-col bg-white m-5 z-30 p-8 ">
@@ -43,7 +53,9 @@ function Product ({ id, title, price, description, category, image }) {
         </div>
       )}
 
-      <button className="mt-auto button">Add to Basket</button>
+      <button
+        onClick={addItemToBasket}
+        className="mt-auto button">Add to Basket</button>
 
     </div>
   )
